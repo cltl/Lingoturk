a60bbf44a2c503feb5778edb8de8759f0e9f4019
PiaSommerauer <pia.sommerauer@live.com>
2019-11-11 09:26:37 +0100

    update

--- lingoturk/app/models/Questions/judgeDescriptionsExperiment/resultQuery.sql
+++ lingoturk/app/models/Questions/judgeDescriptionsExperiment/resultQuery.sql
@@ -3,5 +3,5 @@ SELECT fileName, listNumber, assignmentId, hitId, workerId, origin, timestamp, p
 	LEFT OUTER JOIN Questions USING (QuestionId)
 	LEFT OUTER JOIN Groups USING (PartId)
 ) as tmp
-WHERE LingoExpModelId = 343
+WHERE LingoExpModelId = 1
 ORDER BY partId, questionId, workerId
