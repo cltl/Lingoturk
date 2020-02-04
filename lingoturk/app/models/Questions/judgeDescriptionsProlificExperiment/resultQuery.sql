b78a9169f356ad6e4d2069dac9b18608aa560c3d
PiaSommerauer <pia.sommerauer@live.com>
2020-02-04 08:26:52 +0100

    most recent changes

--- lingoturk/app/models/Questions/judgeDescriptionsProlificExperiment/resultQuery.sql
+++ lingoturk/app/models/Questions/judgeDescriptionsProlificExperiment/resultQuery.sql
@@ -1,7 +1,7 @@
-SELECT workerId, timestamp, partId, questionId, answer::TEXT, (data->>'quid') as quid, (data->>'description') as description, (data->>'subList') as subList, id FROM (
+SELECT fileName, listNumber, assignmentId, hitId, workerId, origin, timestamp, partId, questionId, answer::TEXT, (data->>'quid') as quid, (data->>'description') as description, (data->>'exampleTrue') as exampleTrue, (data->>'exampleFalse') as exampleFalse, (data->>'run') as run, (data->>'subList') as subList, (data->>'completionUrl') as completionUrl, id FROM (
 	(SELECT * FROM Results WHERE experimentType='judgeDescriptionsProlificExperiment') as tmp1
 	LEFT OUTER JOIN Questions USING (QuestionId)
 	LEFT OUTER JOIN Groups USING (PartId)
 ) as tmp
-WHERE LingoExpModelId = 362
+WHERE LingoExpModelId = 125
 ORDER BY partId, questionId, workerId
