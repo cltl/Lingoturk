d1dfcda046f5ed05df4bccad6699a48690b4f1c2
BaranBarbarestani <86965449+BaranBarbarestani@users.noreply.github.com>
2023-02-27 07:00:04 +0100

    Add files via upload

---
+++ lingoturk/app/models/Questions/ContextualInappropriateLanguageDetectionExperiment/resultQuery.sql
@@ -0,0 +1,7 @@
+SELECT fileName, listNumber, assignmentId, hitId, workerId, origin, timestamp, partId, questionId, answer::TEXT, (data->>'comment_id') as comment_id, (data->>'title_id') as title_id, (data->>'community_id') as community_id, (data->>'title_text') as title_text, (data->>'comment_text') as comment_text, (data->>'context') as context, (data->>'comment') as comment, (data->>'comment_number') as comment_number, (data->>'redirectUrl') as redirectUrl, id FROM (
+	(SELECT * FROM Results WHERE experimentType='ContextualInappropriateLanguageDetectionExperiment') as tmp1
+	LEFT OUTER JOIN Questions USING (QuestionId)
+	LEFT OUTER JOIN Groups USING (PartId)
+) as tmp
+WHERE LingoExpModelId = 330
+ORDER BY partId, questionId, workerId
