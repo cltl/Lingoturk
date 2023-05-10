1eaa4986cb430dbadf8a095b4f4ab81c5a8bbab6
BaranBarbarestani <86965449+BaranBarbarestani@users.noreply.github.com>
2023-05-10 11:33:09 +0200

    Add files via upload

--- lingoturk/public/javascripts/ExperimentRendering/InappropriateOrNotExplicitOrImplicitExperiment/InappropriateOrNotExplicitOrImplicitExperiment_render.js
+++ lingoturk/public/javascripts/ExperimentRendering/InappropriateOrNotExplicitOrImplicitExperiment/InappropriateOrNotExplicitOrImplicitExperiment_render.js
@@ -36,7 +36,23 @@
             {name : "Are you bilingual (grown up with more than one language)?", type: "boolean", answer : undefined},
             {name : "Please list the languages you speak at at the advance level.", type: "text", answer : "", optional : true}
         ];
+        self.isAnyCheckboxSelected = function () {
+      for (var i = 0; i < self.questions.length; i++) {
+        if ($scope.RC.questions[i].answer) {
+          return true; // At least one checkbox is selected
+        }
+      }
+      return false; // No checkbox is selected
+    };
 
+    // Add the following function to check if at least one checkbox is selected
+    $scope.isAtLeastOneCheckboxSelected = function (answer) {
+      if (!answer || Object.keys(answer).length === 0) {
+        return false;
+      }
+
+      return Object.values(answer).some(value => value === true);
+    };
         this.resultsSubmitted = function(){
             self.subListsIds.splice(0,1);
             if(self.subListsIds.length > 0 ){
@@ -45,7 +61,6 @@
                 self.processFinish();
             }
         };
-
         this.processFinish = function(){
             if(!self.useGoodByeMessage){
                 self.finished();
@@ -53,21 +68,19 @@
                 self.showMessage = "goodBye";
             }
         };
-
         this.finished = function(){
             if(self.origin == null || self.origin == "NOT AVAILABLE"){
                 bootbox.alert("Results successfully submitted. You might consider redirecting your participants now.");
             }else if(self.origin == "MTURK"){
                 $("#form").submit();
             }else if(self.origin == "PROLIFIC"){
-				if(inIframe()){
+        if(inIframe()){
                     window.top.location.href = self.redirectUrl;
                 }else{
                     window.location = self.redirectUrl;
                 }
             }
         };
-
         this.nextSublist = function(){
             self.questionIndex = 0;
             self.questionNumber = 1;
@@ -79,6 +92,15 @@
             self.failedTries = 0;
             bootbox.alert("An error occurred while submitting your results. Please try again in a few seconds.");
         };
+        this.isAnyCheckboxSelected = function() {
+          for (var i = 0; i < self.questions.length; i++) {
+            if ($scope.RC.questions[i].answer) {
+              return true; // At least one checkbox is selected
+            }
+          }
+          return false; // No checkbox is selected
+        };
+
 
         this.handleError = function(){
             if(self.failedTries < 100){
@@ -88,168 +110,164 @@
                 self.resultSubmissionError();
             }
         };
+self.failedTries = 0;
+this.submitResults = function (successCallback, errorCallback) {
+    var results = {
+        experimentType : "InappropriateOrNotExplicitOrImplicitExperiment",
+        results : self.questions,
+        expId : self.expId,
+        origin : self.origin,
+        statistics : self.statistics,
+        assignmentId : self.assignmentId,
+        hitId : self.hitId,
+        workerId : self.workerId,
+        partId : (self.partId == null ? -1 : self.partId)
+    };
 
-        self.failedTries = 0;
-        this.submitResults = function (successCallback, errorCallback) {
-            var results = {
-                experimentType : "InappropriateOrNotExplicitOrImplicitExperiment",
-                results : self.questions,
-                expId : self.expId,
-                origin : self.origin,
-                statistics : self.statistics,
-                assignmentId : self.assignmentId,
-                hitId : self.hitId,
-                workerId : self.workerId,
-                partId : (self.partId == null ? -1 : self.partId)
-            };
 
-
-            $http.post("/submitResults", results)
-                .success(successCallback)
-                .error(errorCallback);
-        };
-
-        this.next = function(){
-            if(self.state == "workerIdSlide"){
-                if(self.questionId == null && self.partId == null){
-                    self.load(function(){
-                        self.state = self.allStates[++self.slideIndex];
-                    });
-                    return;
-                }
-            }
-
-            if(self.slideIndex + 1 < self.allStates.length){
+    $http.post("/submitResults", results)
+        .success(successCallback)
+        .error(errorCallback);
+};
+this.next = function(){
+    if(self.state == "workerIdSlide"){
+        if(self.questionId == null && self.partId == null){
+            self.load(function(){
                 self.state = self.allStates[++self.slideIndex];
-            }else{
-                self.submitResults(self.resultsSubmitted, self.handleError);
-            }
-        };
-
-        this.nextQuestion = function(){
-            if(self.questionIndex + 1 < self.questions.length){
-                ++self.questionIndex;
-                ++self.questionNumber;
-            }else{
-                self.next();
-            }
-        };
-        this.load = function(callback){
-            var subListMap = self.subListMap;
-
-            if(self.questionId != null){
-                $http.get("/getQuestion/" + self.questionId).success(function (data) {
-                    self.questions = [data];
-
-                    subListMap[self.questions[0].subList] = [self.questions[0]];
-
-                    if(callback !== undefined){
-                        callback();
-                    }
-                });
-            }else if(self.partId != null){
-                $http.get("/returnPart?partId=" + self.partId).success(function (data) {
-                    var json = data;
-                    self.part = json;
-                    self.questions = json.questions;
-
-                    if(self.shuffleQuestions){
-                        shuffleArray(self.part.questions);
-                    }
-
-                    for(var i = 0; i < self.questions.length; ++i){
-                        var q = self.questions[i];
-                        q.comment_text_a = q.comment_text_a.split(' ');
-                        q.comment_text_b = q.comment_text_b.split(' ');
-
-                        if (subListMap.hasOwnProperty(q.subList)){
-                            subListMap[q.subList].push(q);
-                            // assign redirect URL
-                            self.redirectUrl = q.redirectUrl;
-
-                        }else{
-                            subListMap[q.subList] = [q];
-                            self.subListsIds.push(q.subList);
-                            //assign redirect URL
-                            self.redirectUrl = q.redirectUrl
-                        }
-                    }
-                    if(self.shuffleSublists){
-                        shuffleArray(self.subListsIds);
-                    }
-                    self.questions = self.subListMap[self.subListsIds[0]];
-
-                    if(callback !== undefined){
-                        callback();
-                    }
-                });
-            }else{
-                $http.get("/getPart?expId=" + self.expId + "&workerId=" + self.workerId).success(function (data) {
-                    var json = data;
-                    self.part = json;
-                    self.partId = json.id;
-                    self.questions = json.questions;
-
-                    if(self.shuffleQuestions){
-                        shuffleArray(self.part.questions);
-                    }
-
-                    for(var i = 0; i < self.questions.length; ++i){
-                        var q = self.questions[i];
-                        q.comment_text_a = q.comment_text_a.split(' ');
-                        q.comment_text_b = q.comment_text_b.split(' ');
-
-                        if (subListMap.hasOwnProperty(q.subList)){
-                            subListMap[q.subList].push(q);
-                            // assign redirect URL
-                            self.redirectUrl = q.redirectUrl;
-                        }else{
-                            subListMap[q.subList] = [q];
-                            self.subListsIds.push(q.subList);
-                            // assign redirect redirect URL
-                            self.redirectUrl = q.redirectUrl;
-                        }
-                    }
-                    if(self.shuffleSublists){
-                        shuffleArray(self.subListsIds);
-                    }
-                    self.questions = self.subListMap[self.subListsIds[0]];
-
-                    if(callback !== undefined){
-                        callback();
-                    }
-                });
-            }
-        };
-
-        $(document).ready(function () {
-            self.questionId = ($("#questionId").length > 0) ? $("#questionId").val() : null;
-            self.partId = ($("#partId").length > 0) ? $("#partId").val() : null;
-            self.expId = ($("#expId").length > 0) ? $("#expId").val() : null;
-            self.hitId = ($("#hitId").length > 0) ? $("#hitId").val() : "NOT AVAILABLE";
-            self.workerId = ($("#workerId").length > 0) ? $("#workerId").val() : "";
-            self.assignmentId = ($("#assignmentId").length > 0) ? $("#assignmentId").val() : "NOT AVAILABLE";
-            self.origin = ($("#origin").length > 0) ? $("#origin").val() : "NOT AVAILABLE";
-            self.redirectUrl = ($("#redirectUrl").length > 0) ? $("#redirectUrl").val() : null;
-
-            if(self.questionId != null || self.partId != null){
-                self.load();
-            }
-
-            self.allStates = ["instructionsSlide","workerIdSlide","statisticsSlide","questionSlide"];
-
-            if(!self.useStatistics){
-                var index = self.allStates.indexOf("statisticsSlide");
-                self.allStates.splice(index,1);
-            }
-
-            $scope.$apply(self.state = self.allStates[0]);
-
-            $(document).on("keypress", ":input:not(textarea)", function(event) {
-                if (event.keyCode == 13) {
-                    event.preventDefault();
-                }
             });
+            return;
+        }
+    }
+
+    if(self.slideIndex + 1 < self.allStates.length){
+        self.state = self.allStates[++self.slideIndex];
+    }else{
+        self.submitResults(self.resultsSubmitted, self.handleError);
+    }
+};
+this.nextQuestion = function(){
+    if(self.questionIndex + 1 < self.questions.length){
+        ++self.questionIndex;
+        ++self.questionNumber;
+    }else{
+        self.next();
+    }
+};
+this.load = function(callback){
+    var subListMap = self.subListMap;
+
+    if(self.questionId != null){
+        $http.get("/getQuestion/" + self.questionId).success(function (data) {
+            self.questions = [data];
+
+            subListMap[self.questions[0].subList] = [self.questions[0]];
+
+            if(callback !== undefined){
+                callback();
+            }
         });
-    }]);
+    }else if(self.partId != null){
+        $http.get("/returnPart?partId=" + self.partId).success(function (data) {
+            var json = data;
+            self.part = json;
+            self.questions = json.questions;
+
+            if(self.shuffleQuestions){
+                shuffleArray(self.part.questions);
+            }
+
+            for(var i = 0; i < self.questions.length; ++i){
+                var q = self.questions[i];
+                q.comment_text_a = q.comment_text_a.split(' ');
+                q.comment_text_b = q.comment_text_b.split(' ');
+
+                if (subListMap.hasOwnProperty(q.subList)){
+                    subListMap[q.subList].push(q);
+                    // assign redirect URL
+                    self.redirectUrl = q.redirectUrl;
+
+                }else{
+                    subListMap[q.subList] = [q];
+                    self.subListsIds.push(q.subList);
+                    //assign redirect URL
+                    self.redirectUrl = q.redirectUrl
+                }
+            }
+            if(self.shuffleSublists){
+                shuffleArray(self.subListsIds);
+            }
+            self.questions = self.subListMap[self.subListsIds[0]];
+
+            if(callback !== undefined){
+                callback();
+            }
+        });
+    }else{
+        $http.get("/getPart?expId=" + self.expId + "&workerId=" + self.workerId).success(function (data) {
+            var json = data;
+            self.part = json;
+            self.partId = json.id;
+            self.questions = json.questions;
+
+            if(self.shuffleQuestions){
+                shuffleArray(self.part.questions);
+            }
+
+            for(var i = 0; i < self.questions.length; ++i){
+                var q = self.questions[i];
+                q.comment_text_a = q.comment_text_a.split(' ');
+                q.comment_text_b = q.comment_text_b.split(' ');
+
+                if (subListMap.hasOwnProperty(q.subList)){
+                    subListMap[q.subList].push(q);
+                    // assign redirect URL
+                    self.redirectUrl = q.redirectUrl;
+                }else{
+                    subListMap[q.subList] = [q];
+                    self.subListsIds.push(q.subList);
+                    // assign redirect redirect URL
+                    self.redirectUrl = q.redirectUrl;
+                }
+            }
+            if(self.shuffleSublists){
+                shuffleArray(self.subListsIds);
+            }
+            self.questions = self.subListMap[self.subListsIds[0]];
+
+            if(callback !== undefined){
+                callback();
+            }
+        });
+    }
+};
+$(document).ready(function () {
+    self.questionId = ($("#questionId").length > 0) ? $("#questionId").val() : null;
+    self.partId = ($("#partId").length > 0) ? $("#partId").val() : null;
+    self.expId = ($("#expId").length > 0) ? $("#expId").val() : null;
+    self.hitId = ($("#hitId").length > 0) ? $("#hitId").val() : "NOT AVAILABLE";
+    self.workerId = ($("#workerId").length > 0) ? $("#workerId").val() : "";
+    self.assignmentId = ($("#assignmentId").length > 0) ? $("#assignmentId").val() : "NOT AVAILABLE";
+    self.origin = ($("#origin").length > 0) ? $("#origin").val() : "NOT AVAILABLE";
+    self.redirectUrl = ($("#redirectUrl").length > 0) ? $("#redirectUrl").val() : null;
+
+    if(self.questionId != null || self.partId != null){
+        self.load();
+    }
+
+    self.allStates = ["instructionsSlide","workerIdSlide","statisticsSlide","questionSlide"];
+
+    if(!self.useStatistics){
+        var index = self.allStates.indexOf("statisticsSlide");
+        self.allStates.splice(index,1);
+    }
+
+    $scope.$apply(self.state = self.allStates[0]);
+
+    $(document).on("keypress", ":input:not(textarea)", function(event) {
+        if (event.keyCode == 13) {
+            event.preventDefault();
+        }
+    });
+});
+}]);
 })();
