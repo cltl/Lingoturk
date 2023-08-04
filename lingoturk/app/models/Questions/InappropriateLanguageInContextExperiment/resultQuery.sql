6c71649d8b0859a00fa723b244e66bf5f667e02f
BaranBarbarestani <86965449+BaranBarbarestani@users.noreply.github.com>
2023-08-04 12:19:47 +0200

    Delete lingoturk/app/models/Questions/InappropriateLanguageInContextExperiment directory

--- lingoturk/app/models/Questions/InappropriateLanguageInContextExperiment/resultQuery.sql
+++
@@ -1,7 +0,0 @@
-SELECT fileName, listNumber, assignmentId, hitId, workerId, origin, timestamp, partId, questionId, answer::TEXT, (data->>'comment_id') as comment_id, (data->>'title_id') as title_id, (data->>'community_id') as community_id, (data->>'title_text') as title_text, (data->>'comment_text') as comment_text, (data->>'context') as context, (data->>'comment') as comment, (data->>'redirectUrl') as redirectUrl, id FROM (
-	(SELECT * FROM Results WHERE experimentType='InappropriateLanguageInContextExperiment') as tmp1
-	LEFT OUTER JOIN Questions USING (QuestionId)
-	LEFT OUTER JOIN Groups USING (PartId)
-) as tmp
-WHERE LingoExpModelId = 327
-ORDER BY partId, questionId, workerId
