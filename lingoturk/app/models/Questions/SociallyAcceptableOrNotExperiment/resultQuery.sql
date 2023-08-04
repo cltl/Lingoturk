7400c4047120bcc2c0b66ff521fed44744cb933c
BaranBarbarestani <86965449+BaranBarbarestani@users.noreply.github.com>
2023-08-04 12:20:52 +0200

    Delete lingoturk/app/models/Questions/SociallyAcceptableOrNotExperiment directory

--- lingoturk/app/models/Questions/SociallyAcceptableOrNotExperiment/resultQuery.sql
+++
@@ -1,7 +0,0 @@
-SELECT fileName, listNumber, assignmentId, hitId, workerId, origin, timestamp, partId, questionId, answer::TEXT, (data->>'comment_ids') as comment_ids, (data->>'title_id') as title_id, (data->>'community_id') as community_id, (data->>'title_text') as title_text, (data->>'comment_text') as comment_text, (data->>'redirectUrl') as redirectUrl, id FROM (
-	(SELECT * FROM Results WHERE experimentType='SociallyAcceptableOrNotExperiment') as tmp1
-	LEFT OUTER JOIN Questions USING (QuestionId)
-	LEFT OUTER JOIN Groups USING (PartId)
-) as tmp
-WHERE LingoExpModelId = 281
-ORDER BY partId, questionId, workerId
