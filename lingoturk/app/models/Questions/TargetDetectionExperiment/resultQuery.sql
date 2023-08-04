fe02cdb1dad7e3b56875ed3d6d00392e72dddbc2
BaranBarbarestani <86965449+BaranBarbarestani@users.noreply.github.com>
2023-08-04 12:20:30 +0200

    Delete lingoturk/app/models/Questions/TargetDetectionExperiment directory

--- lingoturk/app/models/Questions/TargetDetectionExperiment/resultQuery.sql
+++
@@ -1,7 +0,0 @@
-SELECT fileName, listNumber, assignmentId, hitId, workerId, origin, timestamp, partId, questionId, answer::TEXT, (data->>'id') as id, (data->>'dataset') as dataset, (data->>'text') as text, (data->>'tokens') as tokens, (data->>'label') as label, id FROM (
-	(SELECT * FROM Results WHERE experimentType='TargetDetectionExperiment') as tmp1
-	LEFT OUTER JOIN Questions USING (QuestionId)
-	LEFT OUTER JOIN Groups USING (PartId)
-) as tmp
-WHERE partid = 102
-ORDER BY partId, questionId, workerId
